30a087268731cd6dbcea439596a1b2c608f54c12
Jack5496 <mllerpeter68@yahoo.de>
2016-11-05 17:16:29 +0100

    Update PA1.c

--- PA1.c
+++ PA1.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-char backslash[] = "\";
+char path_to_dictionary[] = "rfc793.txt";
+char path_to_passfile[] = "passfile";
 
 int main(int argc, char **argv){
+	printf("\n");
+	prinf("Starting Passwordfinder\n");
+	printf("%d",argc);
 	for(int i=0; i<argc; i++){
-		printf("%s",argv[i]);
+		printf("%s \n",argv[i]);
 	}
 }
