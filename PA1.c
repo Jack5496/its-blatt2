11148e601f44decae3723a28ed18b8203922b01e
Jack5496 <mllerpeter68@yahoo.de>
2016-11-06 15:05:59 +0100

    Update PA1.c

--- PA1.c
+++ PA1.c
@@ -69,6 +69,7 @@ int savePassBase(){
 
 int checkIfBase64SHA1Matches(char word[], char base[]){
 		int comp = strcmp(passBase,base);
+		printf("Word: %s Base: %s",word,base);
 	
 		if(comp==0){
 			printf("%s: %s",word,pass_file_line);	
