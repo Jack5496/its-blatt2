a97fdd7d8aa08e9508d946dc891a734d45e918a6
Jack5496 <mllerpeter68@yahoo.de>
2016-11-06 16:24:10 +0100

    Update PA1.c

--- PA1.c
+++ PA1.c
@@ -83,7 +83,7 @@ int checkIfIsPassword(char word[]){
 	//word[strlen(word)-1]='\0';
 	
 	printf("Wordlength: %d\n",strlen(word));
-	SHA1(word,strlen(word),hash);
+	SHA1(word,strlen(word)+1,hash);
 	
 	unsigned char base[29];
 	b64sha1(hash,base);
