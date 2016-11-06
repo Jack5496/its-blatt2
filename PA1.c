f34d8e948ce3912f45112de4cc972ddf1626743f
Jack5496 <mllerpeter68@yahoo.de>
2016-11-06 17:22:32 +0100

    Update PA1.c

--- PA1.c
+++ PA1.c
@@ -60,7 +60,7 @@ int savePassBase(){
 			memcpy(passBase, &line[startpos],length-1);
 			passBase[length-1]='\0';
 			pass_file_line = malloc((strlen(line))*sizeof(char));
-			memcpy(pass_file_line, &line[0],strlen(line));
+			memcpy(pass_file_line, &line[0],strlen(line)-1);
 		}
 	}
 	
