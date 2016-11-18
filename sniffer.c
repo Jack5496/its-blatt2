568223f1b9dc8e6d730d877cad8cc0ba3ad33ecf
NilsBaumgartner <mllerpeter68@yahoo.de>
2016-11-18 18:44:58 +0100

    Main Init

---
+++ sniffer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+* Die Main Function in der Das Programm einsetzt
+**/
+int main(int argc, char **argv){
+	printf("\n");
+		
+	
+	return 0;
+}
