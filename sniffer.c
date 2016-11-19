28910b918d05ed56b588cf2df9ab242851c1a33a
NilsBaumgartner <mllerpeter68@yahoo.de>
2016-11-19 01:08:55 +0100

    Update sniffer.c

--- sniffer.c
+++ sniffer.c
@@ -105,7 +105,7 @@ void print_tcp_packet(unsigned char* Buffer, int Size)
     fprintf(logfile,"\n");
  
     fprintf(logfile,"Data Payload\n");  
-    PrintData(data_payload , (Size - &tcph->doff*4-&iph->ihl*4) );
+    PrintData(data_payload , (Size - tcph->doff*4-iph*->ihl*4) );
                          
     fprintf(logfile,"\n###########################################################");
 }
